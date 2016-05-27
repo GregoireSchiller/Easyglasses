11f79293f6e4dbb37f8eb90effb9f355471aedec
Benjamin Meuleman <benjamin.meuleman@gmail.com>
2016-05-27 12:42:40 +0200

    drag to resize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui/resizable
 //= require bootstrap-sprockets
 //= require_tree .
---
+++ app/assets/javascripts/resizable.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  grid_size = 10;
+  $(".box").resizable({ grid: grid_size * 2 });
+});
